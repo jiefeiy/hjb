b62b16b57d5190d2bd2a35f0dd261eda4c08668b
jiefeiy <jiefei.yang01@gmail.com>
2024-05-07 19:55:06 +0800

    3d difference

--- a2_fully_nonlinear_3d.m
+++ a2_fully_nonlinear_3d.m
@@ -8,7 +8,14 @@ clear, clc
 % 
 
 %% parameter setting 
-sigma_min = 1;
-sigma_max = sqrt(2);
-T = 0.5;
-x0 = [5 6 7]';
+% sigma_min = 1;
+% sigma_max = sqrt(2);
+% T = 0.5;
+% x0 = [5 6 7]';
+
+%% 2d finite difference
+n = 4;
+e = ones(n,1);
+T = spdiags([-e 2*e -e], -1:1, n, n);
+
+
