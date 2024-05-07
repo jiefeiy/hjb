38da4391da5a43f6531fad7bee920741e009e4da
jiefeiy <jiefei.yang01@gmail.com>
2024-05-07 14:29:53 +0800

    init 3d nonlinear

---
+++ a2_fully_nonlinear_3d.m
@@ -0,0 +1,14 @@
+clear, clc
+% numerical tests:
+% [1] W Guo, J Zhang, J Zhuo. (2015) A monotone scheme for high-dimensional
+% fully nonlinear pdes.
+%
+% test with example 1 in Section 6 Numerical examples. 
+%
+% 
+
+%% parameter setting 
+sigma_min = 1;
+sigma_max = sqrt(2);
+T = 0.5;
+x0 = [5 6 7]';
