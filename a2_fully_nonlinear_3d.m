793fa30fc06e21a57f00696b29eaffb552c374a7
jiefeiy <jiefei.yang01@gmail.com>
2024-05-12 20:52:49 +0800

    3d naive

--- a2_fully_nonlinear_3d.m
+++ a2_fully_nonlinear_3d.m
@@ -4,18 +4,16 @@ clear, clc
 % fully nonlinear pdes.
 %
 % test with example 1 in Section 6 Numerical examples. 
+% finite difference method with naive implementation.
 
 %% parameter setting 
 sigma_min = 1;
 sigma_max = sqrt(2);
 T = 0.5;
-x0 = [5 6 7]';
-x0 = mod(x0, 2*pi);
-sin(sum(x0))
 
 %% discritization and terminal condition
 n_time = 2000;
-nx = 30;
+nx = 150;
 dt = T / n_time;
 h = 2*pi / nx;
 x1_grid = linspace(0, 2*pi, nx+1);
@@ -24,8 +22,14 @@ x3_grid = linspace(0, 2*pi, nx+1);
 [X1, X2, X3] = ndgrid(x1_grid, x2_grid, x3_grid);
 u = sin(T + X1 + X2 + X3);
 
+x0 = [5;6;7];
+% id_grid = [20 20 5];
+% x0 = [x1_grid(id_grid(1)); x2_grid(id_grid(2)); x3_grid(id_grid(3))];
+x0 = mod(x0, 2*pi);
+sin(sum(x0))
+
 %% matrix free implementation
-sigma = sigma_max;
+sigma = sigma_min;
 for t = 1:n_time
     unew = zeros(nx+1, nx+1, nx+1);
     for i = 1:nx+1
@@ -52,57 +56,23 @@ for t = 1:n_time
                         else
                             km = k-1; kp = k+1;
                         end
-    
-                         unew(i,j,k) = u(i,j,k) + dt/(3*2*h)*(u(ip,j,k) - u(im,j,k) + u(i,jp,k) - u(i,jm,k)...
-                             + u(i,j,kp) - u(i,j,km)) + dt/2*sigma^2/(h^2)*(u(ip,j,k) + u(im,j,k)...
-                             + u(i,jp,k) + u(i,jm,k) + u(i,j,kp) + u(i,j,km) - 6*u(i,j,k)) ...
-                             - dt*3/2*sigma^2 * u(i,j,k);
+                        
+                        laplace_u = (u(ip,j,k) + u(im,j,k)...
+                             + u(i,jp,k) + u(i,jm,k) + u(i,j,kp) + u(i,j,km) - 6*u(i,j,k));
+ 
+                        unew(i,j,k) = u(i,j,k) - dt/(3*2*h)*(u(ip,j,k) - u(im,j,k) + u(i,jp,k) - u(i,jm,k)...
+                             + u(i,j,kp) - u(i,j,km)) + dt/(2*h*h)*(sigma_max^2*max(laplace_u,0) - sigma_min^2*max(-laplace_u,0)) ...
+                             + dt*3/2*(sigma_min^2*max(u(i,j,k),0) - sigma_max^2*max(-u(i,j,k),0));
                 end
         end
     end
     u = unew;
 end
 
-X1t = pagetranspose(X1);
-X2t = pagetranspose(X2);
-X3t = pagetranspose(X3); % convert to meshgrid
-ut = pagetranspose(u);
-Vq = interp3(X1t,X2t,X3t,ut,x0(1),x0(2),x0(3));
-Vq
+% u(id_grid(1), id_grid(2), id_grid(3))
 
-
-% [X1m,X2m,X3m] = meshgrid(x1_grid, x2_grid, x3_grid);
-% isequal(X1t,X1m) & isequal(X2t,X2m) & isequal(X3t,X3m)
-
-% %% inner grid
-% i = 2:nx; j=i; k=i;
-% sigma = sigma_min;
-% unew = u;
-% unew(i,j,k) = u(i,j,k) + dt/(3*2*h)*(u(i+1,j,k) - u(i-1,j,k) + u(i,j+1,k) - u(i,j-1,k) + u(i,j,k+1) - u(i,j,k-1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(i+1,j,k) + u(i-1,j,k) + u(i,j+1,k) + u(i,j-1,k) + u(i,j,k+1) + u(i,j,k-1) - 6*u(i,j,k)) ...
-%     - dt*3/2*sigma^2 * u(i,j,k);
-% 
-% %% periodic boundary condition
-% unew(1,j,k) = u(1,j,k) + dt/(3*2*h)*(u(2,j,k) - u(nx+1,j,k) + u(1,j+1,k) - u(1,j-1,k) + u(1,j,k+1) - u(1,j,k-1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(2,j,k) + u(nx+1,j,k) + u(1,j+1,k) + u(1,j-1,k) + u(1,j,k+1) + u(1,j,k-1) - 6*u(1,j,k)) ...
-%     - dt*3/2*sigma^2 * u(1,j,k);
-% unew(nx+1,j,k) = u(nx+1,j,k) + dt/(3*2*h)*(u(1,j,k) - u(nx,j,k) + u(nx+1,j+1,k) - u(nx+1,j-1,k) + u(nx+1,j,k+1) - u(nx+1,j,k-1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(1,j,k) + u(nx,j,k) + u(nx+1,j+1,k) + u(nx+1,j-1,k) + u(nx+1,j,k+1) + u(nx+1,j,k-1) - 6*u(nx+1,j,k)) ...
-%     - dt*3/2*sigma^2 * u(nx+1,j,k);
-% 
-% unew(i,1,k) = u(i,1,k) + dt/(3*2*h)*(u(i+1,1,k) - u(i-1,1,k) + u(i,2,k) - u(i,nx+1,k) + u(i,1,k+1) - u(i,1,k-1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(i+1,1,k) + u(i-1,1,k) + u(i,2,k) + u(i,nx+1,k) + u(i,1,k+1) + u(i,1,k-1) - 6*u(i,1,k)) ...
-%     - dt*3/2*sigma^2 * u(i,1,k);
-% unew(i,nx+1,k) = u(i,nx+1,k) + dt/(3*2*h)*(u(i+1,nx+1,k) - u(i-1,nx+1,k) + u(i,1,k) - u(i,nx,k) + u(i,nx+1,k+1) - u(i,nx+1,k-1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(i+1,nx+1,k) + u(i-1,nx+1,k) + u(i,1,k) + u(i,nx,k) + u(i,nx+1,k+1) + u(i,nx+1,k-1) - 6*u(i,nx+1,k)) ...
-%     - dt*3/2*sigma^2 * u(i,nx+1,k);
-% 
-% unew(i,j,1) = u(i,j,1) + dt/(3*2*h)*(u(i+1,j,1) - u(i-1,j,1) + u(i,j+1,1) - u(i,j-1,1) + u(i,j,2) - u(i,j,nx+1)) ...
-%     + dt/2*sigma^2/(h^2)*(u(i+1,j,1) + u(i-1,j,1) + u(i,j+1,1) + u(i,j-1,1) + u(i,j,2) + u(i,j,nx+1) - 6*u(i,j,1)) ...
-%     - dt*3/2*sigma^2 * u(i,j,1);
-% unew(i,j,nx+1) = u(i,j,nx+1) + dt/(3*2*h)*(u(i+1,j,nx+1) - u(i-1,j,nx+1) + u(i,j+1,nx+1) - u(i,j-1,nx+1) + u(i,j,1) - u(i,j,nx)) ...
-%     + dt/2*sigma^2/(h^2)*(u(i+1,j,nx+1) + u(i-1,j,nx+1) + u(i,j+1,nx+1) + u(i,j-1,nx+1) + u(i,j,1) + u(i,j,nx) - 6*u(i,j,nx+1)) ...
-%     - dt*3/2*sigma^2 * u(i,j,nx+1);
+uq = interpn(X1,X2,X3,u,x0(1),x0(2),x0(3));
+uq
 
 
 
